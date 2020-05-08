bfb860972b2e10be5702e9fb4e3cb6de0e325023
Moses A. Samuel <mosamuel98@gmail.com>
2020-05-08 10:11:36 -0700

    complete ex4.py

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,12 @@
+# To speed up the second test, you can reduce the range of the loop from 5 million to 500. 
+# If it works with 500 items, it should work with 5 million as well.
 def has_negatives(a):
-
-    """
-    YOUR CODE HERE
-    """
-
+    postivie_nums = {}
+    for num in a:
+        if num > 0 and num not in postivie_nums:
+            postivie_nums[num] = False
+    result = list(filter(lambda num: num * -1 in a, postivie_nums.keys()))
+    
     return result
 
 
